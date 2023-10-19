660e2620b07f83197fbd736f29e2adf73462d62d
bocchiko <124472850+bocchiko@users.noreply.github.com>
2023-10-18 18:01:04 -0600

    Formulario.js

--- paises/App.js
+++ paises/App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Formulario from './src/components/Formulario';
 
 export default function App() {
+  const [busqueda, guardarbusqueda] = useState({pais: ''})
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <View style={styles.contenido}>
+        <Formulario
+          busqueda={busqueda}
+          guardarbusqueda={guardarbusqueda}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'rgb(71,149,212)',
     justifyContent: 'center',
   },
+  contenido: {
+    margin: "2.5%",
+  }
 });
