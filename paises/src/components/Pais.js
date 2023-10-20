180c60cd5f29f1af597dd9ff3ad735c4b8103ff9
bocchiko <luisbonilla0908@gmail.com>
2023-10-20 14:54:55 -0600

    Muestra bandera de pais

--- paises/src/components/Pais.js
+++ paises/src/components/Pais.js
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {Card} from "react-native-elements";
-import {Text, View} from "react-native";
+import {Text, View, Image} from "react-native";
 
 const Pais = ({ resultado }) => {
     const [info, setInfo] = useState([]);
@@ -9,7 +9,13 @@ const Pais = ({ resultado }) => {
     const [region, setRegion] = useState();
     const [lengua, setLengua] = useState([]);
     const [km2, setKm2] = useState([]);
-    const [bandera, setBandera] = useState("");
+    const [banderaURL, setBanderaURL] = useState();
+    const [codigoPais, setCodigoPais] = useState("");
+
+    const construirURLBandera = (codigoPais) => {
+        const codigoEnMinusculas = codigoPais.toLowerCase();
+        return `https://flagcdn.com/32x24/${codigoEnMinusculas}.png`;
+      };
 
     useEffect(() => {
         setInfo(resultado);
@@ -19,6 +25,9 @@ const Pais = ({ resultado }) => {
             setCapital(item.governo.capital.nome);
             setRegion(item.localizacao.regiao.nome);
             setKm2(`${item.area.total} ${item.area.unidade.símbolo}`);
+
+            const codigoPais = item.id["ISO-3166-1-ALPHA-2"];
+            setCodigoPais(codigoPais);
 
             Object.values(item.linguas).map((item) => {
                 lengua.push(item.nome);
@@ -36,6 +45,10 @@ const Pais = ({ resultado }) => {
                 <Text>Region: {region}</Text>
                 <Text>Lenguas: {lengua.toString()}</Text>
                 <Text>Kilómetros Cuadrados: {km2}</Text>
+                <Image
+                    source={{ uri: construirURLBandera(codigoPais) }}
+                    style={{ width: 32, height: 24 }}
+                />
             </View>
         </Card>
     );
